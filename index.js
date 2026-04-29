f6dfff305b110901c971906f49d5c1abefce52b8
unknown <anugraharani164@gmail.com>
2026-04-29 12:59:12 +0530

    Server changed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chatapp-blond-six.vercel.app/",
+    origin: "https://chatapp-blond-six.vercel.app",
     methods: ["GET", "POST"],
   },
 });
